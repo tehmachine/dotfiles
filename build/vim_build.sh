bad8215c59c05732fec6cc54c789a0649e5b02f0
Mihai Stroe <saas@emailuser.net>
2021-03-14 20:38:25 +0200

    Change cpu core count for build src

--- build/vim_build.sh
+++ build/vim_build.sh
@@ -11,6 +11,6 @@ cd vim;
 ./configure --with-features=huge --enable-luainterp=yes --enable-python3interp --enable-rubyinterp --with-lua-prefix=/usr/include/lua5.3 --with-python-config-dir=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu;
 make;
 sudo make uninstall;
-sudo make install -j 8;
+sudo make install -j 4;
 cd ~;
 rm -rf ~/vim_temp;
